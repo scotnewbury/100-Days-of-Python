e4d7220394b720429e1b51c2a82bc33634f3fe1a
Scot Newbury <scot@scotnewbury.com>
2025-05-15 01:35:44 -0400

    Completed exercise 3 - swapping variable values

---
+++ day-1/coding-exercise-3.py
@@ -0,0 +1,21 @@
+# Variables
+
+# We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. 
+# Write 3 lines of code to switch the contents of the variables. 
+# You are not allowed to type the words "milk" or "juice". 
+# You are only allowed to use variables to solve this exercise.
+
+glass1 = "milk"
+glass2 = "juice"
+
+# Exercise code
+# glass3 = glass1
+# glass1 = glass2
+# glass2 = glass3
+
+# tuple unpacking version - also passed exercise testing
+glass1,glass2 = glass2,glass1
+
+# Added to confirm - not required
+print(glass1)
+print(glass2)
--- day-1/task.py
+++ day-1/task.py
@@ -1,4 +1,17 @@
-print("Hello world!\nHello world!\nHello world!")
+# Simple print statement with included new lines
+# print("Hello world!\nHello world!\nHello world!")
 
 # Concatenation of strings
-print("Hello " + "Scot")
+# print("Hello " + "Scot")
+
+# Input function
+# Embedded input in a print statement
+# print("Hello " + input("What is your name? ") + "!")
+
+# Lesson 9 - variables
+# name = input("What is your name? ")
+# print ("Hello " + name)
+
+username = input("What is your name? ")
+length = len(username)
+print(length)
