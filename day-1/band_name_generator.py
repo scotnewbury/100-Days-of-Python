ed502f7eb038b421e5a2bdd9985eb636c8ea595e
Scot Newbury <scot@scotnewbury.com>
2025-05-15 01:44:36 -0400

    Completed day 1 quiz - band name generator

---
+++ day-1/band_name_generator.py
@@ -0,0 +1,8 @@
+# Day 1 Final Project - Band Name Generator
+# Two inputs from user - city they grew up in and the name of their pet
+# Combine them in the output for the band name
+
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pets_name = input ("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pets_name)
