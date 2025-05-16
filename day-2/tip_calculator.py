858df0525914b4b87f21b19c3896a0c81cf2fa77
Scot Newbury <scot@scotnewbury.com>
2025-05-16 00:59:23 -0400

    completed tip calculator

--- day-2/tip_calculator.py
+++ day-2/tip_calculator.py
@@ -5,9 +5,14 @@
 # output - the total each person would pay
 
 print("Welcome tto the tip calculator!")
-bill_total = input("What was the total bill? $")
-tip_amount = input("How much tip would you like to give? 10, 12, or 15? ")
-party_size = input("How many pepole to split the bill? ")
+bill_total = float(input("What was the total bill? $"))
+tip_amount = int(input("How much tip would you like to give? 10, 12, or 15? "))
+party_size = int(input("How many pepole to split the bill? "))
 
-per_person_amount = 5
+bill_total_with_tip = bill_total * (1 + (tip_amount / 100))
+
+print(bill_total_with_tip)
+
+per_person_amount = round((bill_total_with_tip / party_size),2)
+
 print(f"Each person should pay: ${per_person_amount}")
