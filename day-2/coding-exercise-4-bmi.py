2688b101cf36285b4133583357f5a935b545316a
Scot Newbury <scot@scotnewbury.com>
2025-05-16 00:33:31 -0400

    Enhanced the BMI calcualtor by adding inputs and output a sentence not just a number

--- day-2/coding-exercise-4-bmi.py
+++ day-2/coding-exercise-4-bmi.py
@@ -1,9 +1,14 @@
-height = 1.65
-weight = 84
+# Enhanced the BMI calculator but asked the user for their height and weight
+# then perform the same calcualtion and out put the result
+
+# input values needed to be cast as floats so that the calucation could be done
+height = float(input("What is your hieght in meters? "))
+weight = float(input("What is your weight in kilograms? "))
 
 # Write you code here.
 # Calculate the bmi using weight and height
 
 bmi = weight / (height ** 2)
 
-print(bmi)
+# When pringint a number just use a comma, no plus symbol
+print("Your BMI is: ",bmi)
