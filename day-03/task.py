bcff099558ad35d423b7d2bbe7ab6a7db8129377
Scot Newbury <scot@scotnewbury.com>
2025-05-17 15:03:47 -0400

    Updated directory structure, started lesson 22

---
+++ day-03/task.py
@@ -0,0 +1,11 @@
+# Conditional statements
+# if/else
+
+print("Welcome to the rolloercoaster!")
+
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can buy a ticket")
+else:
+    print("Sorry you can't ride")
