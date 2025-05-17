3c6248019f1ede46917de1620d9c67c5e3b4286c
Scot Newbury <scot@scotnewbury.com>
2025-05-17 17:42:23 -0400

    Completed up through lesson 25

--- day-03/task.py
+++ day-03/task.py
@@ -25,18 +25,43 @@
 
 # Lesson 24 - nested if statments and elif statements
 
+# print("Welcome to the rolloercoaster!")
+
+# height = int(input("What is your height in cm? "))
+# age = int(input("How old are you? "))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     if age < 12:
+#         print("Your ticket price is $5")
+#     elif age <= 18:
+#         print("Your tickket price is $7")
+#     else:
+#         print("Your ticket price is $12")
+# else:
+#     print("Sorry you can't ride")
+
+# Lesson 25 Multiple if Statements in Succession
 print("Welcome to the rolloercoaster!")
 
 height = int(input("What is your height in cm? "))
 age = int(input("How old are you? "))
+ticket_total = 0  # initialize ticket total
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     if age < 12:
-        print("Your ticket price is $5")
+        print("Child tickets are $5.")
+        ticket_total += 5
     elif age <= 18:
-        print("Your tickket price is $7")
+        print("Youth tickets are $7.")
+        ticket_total += 7
     else:
-        print("Your ticket price is $12")
+        print("Adult tickets are $12.")
+        ticket_total += 12
+    wants_photo = input("Would you like a photo? Type 'y' for Yes and 'n' for No.")
+    if wants_photo == 'y':
+        ticket_total += 3
+    print(f"Your total ticket price is: ${ticket_total}")
 else:
     print("Sorry you can't ride")
