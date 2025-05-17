ff5ad6867d71847c3508eaf2ca293e827fc9d768
Scot Newbury <scot@scotnewbury.com>
2025-05-17 15:36:32 -0400

    Completed lesson 23 - modulo

--- day-03/task.py
+++ day-03/task.py
@@ -1,11 +1,24 @@
+# Lesson 22
 # Conditional statements
 # if/else
 
-print("Welcome to the rolloercoaster!")
+# print("Welcome to the rolloercoaster!")
 
-height = int(input("What is your height in cm? "))
+# height = int(input("What is your height in cm? "))
 
-if height >= 120:
-    print("You can buy a ticket")
+# if height >= 120:
+#     print("You can buy a ticket")
+# else:
+#     print("Sorry you can't ride")
+
+# Lesson 23 - Modulo
+# Take as input from a user, an number, cast it as an integer
+# and determine if it's odd or even
+# If it's odd, print "Odd" otherwise print "Even"
+
+number = int(input("Please enter a number: "))
+
+if number % 2:    # Using the remainder without evaluation
+    print("Odd")    # The modulo result was 1 so the number is odd
 else:
-    print("Sorry you can't ride")
+    print("Even")   # The modulo result was 0 so the number if even
