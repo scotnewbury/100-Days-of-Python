bba126a1e005a802ef4a009a173e962f71e904d0
Scot Newbury <scot@scotnewbury.com>
2025-05-19 00:51:03 -0400

    Completed rock-paper-scissors

--- day-04/paper_rock_scissors.py
+++ day-04/paper_rock_scissors.py
@@ -26,9 +26,26 @@ scissors = '''
 ---.__(___)
 '''
 
+game_images = [rock, paper, scissors]
 print("What do you choose?")
-player_choice = input("Type 0 for Rock, 1 for Paper, or 2 for Scissors.")
+player_choice = int(input("Type 0 for Rock, 1 for Paper, or 2 for Scissors: "))
 computer_choice = random.randint(0, 2)
 
-print(player_choice)
-print(computer_choice)
+if player_choice >= 0 and player_choice < 3:
+    print(f"You chose: {game_images[player_choice]}")
+
+print(f"Computer chose: {game_images[computer_choice]}")
+
+# Player chose rock
+if player_choice == computer_choice:
+    print("It's a draw!")
+elif player_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif player_choice == 1 and computer_choice == 0:
+    print("You win!")
+elif player_choice == 2 and computer_choice == 1:
+    print("You win!")
+elif player_choice < 0 or player_choice > 2:
+    print("You entered an invalid number - you lose!")
+else:
+    print("You lose!")
