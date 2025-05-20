84904a093f653ec8ed2a254735cb97a881749442
Scot Newbury <scot@scotnewbury.com>
2025-05-20 01:55:29 -0400

    Completed pypassword challenge, both levels

--- day-05/pypassword.py
+++ day-05/pypassword.py
@@ -9,12 +9,61 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
 
+# Easy Level
+# my_password = ""
+# for letter in range(1, nr_letters + 1):
+#     my_password += random.choice(letters)
+# for symbol in range(1, nr_symbols + 1):
+#     my_password += random.choice(symbols)
+# for number in range(1, nr_numbers + 1):
+#     my_password += random.choice(numbers)
+
+# print(my_password)
+
+# Hard Level version 1
+# password_length = int(nr_letters + nr_numbers + nr_symbols)
+# print(password_length)
+# my_password = ""
+
+# for password_character in range(1, password_length + 1):
+#     character_type = random.randint(1, 3)
+#     if character_type == 1 and nr_letters > 0:
+#         my_password += random.choice(letters)
+#         print(my_password)
+#         nr_letters -= 1
+#     elif character_type == 2 and nr_symbols > 0:
+#         my_password += random.choice(symbols)
+#         print(my_password)
+#         nr_symbols -= 1
+#     elif character_type == 3 and nr_numbers > 0:
+#         my_password += random.choice(numbers)
+#         print(my_password)
+#         nr_numbers -= 1
+
+# Hard version 2
+# Version 1 worked but the hint suggest shuffle
+
 my_password = ""
-for letter in range(1, nr_letters + 1):
+
+for letter in range(0, nr_letters):
     my_password += random.choice(letters)
-for symbol in range(1, nr_symbols + 1):
+for symbol in range(0, nr_symbols):
     my_password += random.choice(symbols)
-for number in range(1, nr_numbers + 1):
+for number in range(0, nr_numbers):
     my_password += random.choice(numbers)
 
+# We have all the characters in a single string
+# Let's turn them into a list
+char_list = list(my_password)
+
+# Now we shuffle the characters
+random.shuffle(char_list)
+
+# Now we put it back together as a string
+my_password = "".join(char_list)
+
+# Print the final password
 print(my_password)
+
+# Angela dropped everyting into a list, shuffled it,
+# then she used a for loop to build the string
