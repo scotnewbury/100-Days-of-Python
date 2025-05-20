c00a1cf1f3422dd111393c182c669fc87201e8de
Scot Newbury <scot@scotnewbury.com>
2025-05-20 01:07:28 -0400

    Updated code to match PEP-8 requirements

--- day-05/task.py
+++ day-05/task.py
@@ -2,7 +2,8 @@
 # lesson 39 - High Score
 # Use basic for loop, conditionals, etc to replicate the mas() function
 
-student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199,
+                  78, 65, 89, 86, 55, 91, 64, 89]
 
 print(max(student_scores))
 
